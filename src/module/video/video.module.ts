60b555b2b04c0563a6ceeeb14d2e96e2fc32cbf7
Akmaljon571 <ahmadjonovakmal079@gmail.com>
2023-02-23 19:22:45 +0500

    finish: middlewareToken

--- src/module/video/video.module.ts
+++ src/module/video/video.module.ts
@@ -1,9 +1,14 @@
+import { UserEntity } from 'src/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TokenMiddleware } from './../../middleware/middleware.service';
 import { Module } from '@nestjs/common';
 import { VideoService } from './video.service';
 import { VideoController } from './video.controller';
+import { MiddlewareModule } from 'src/middleware/middleware.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity]), MiddlewareModule],
   controllers: [VideoController],
-  providers: [VideoService],
+  providers: [VideoService, TokenMiddleware],
 })
 export class VideoModule {}
---
+++ src/utils/jwt.ts
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv';
+import * as jwt from 'jsonwebtoken';
+dotenv.config();
+
+class JwtStrategy {
+  private readonly JWT_SECRET = process.env.SECRET_KEY;
+
+  sign(payload: any): string {
+    return jwt.sign(payload, this.JWT_SECRET);
+  }
+
+  verify(token: string): any {
+    try {
+      const decoded = jwt.verify(token, this.JWT_SECRET);
+      return decoded;
+    } catch (error) {
+      throw new Error('Invalid token');
+    }
+  }
+}
+
+export default new JwtStrategy();
