d864e493fc7176c810466a40af61a912b11d3275
JumaboyevJasurbek <jumaboyevjasurbek2004@gmail.com>
2023-02-27 23:55:06 +0500

    get create courses added

--- src/module/courses/dto/update-course.dto.ts
+++ src/module/courses/dto/update-course.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 import { CreateCourseDto } from './create-course.dto';
 
-export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
+export class UpdateCourseDto extends PartialType(CreateCourseDto) {
+  @IsString()
+  course_title: string;
+
+  @IsString()
+  course_description: string;
+  @IsString()
+  course_price: string;
+  @IsNumber()
+  course_sequence: number;
+}
