946f101ed42b46570bdc6241a94c90b60f244aec
Akmaljon571 <ahmadjonovakmal079@gmail.com>
2023-03-01 10:46:58 +0500

    fix bug in take users

--- src/module/user-take-book/user-take-book.controller.ts
+++ src/module/user-take-book/user-take-book.controller.ts
@@ -1,19 +1,10 @@
 import { TokenMiddleware } from './../../middleware/middleware.service';
 import { HttpStatus } from '@nestjs/common/enums';
-
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UserTakeBookService } from './user-take-book.service';
-import { Headers, HttpCode, Param, Res } from '@nestjs/common/decorators';
-import { Response } from 'express';
-import {
-  ApiBadRequestResponse,
-  ApiHeader,
-  ApiNotFoundResponse,
-  ApiTags,
-} from '@nestjs/swagger';
+import { Headers, HttpCode } from '@nestjs/common/decorators';
 
-@Controller('user-take-workbook')
-@ApiTags('User Take Workbook')
+@Controller('userTakeBook')
 export class UserTakeBookController {
   constructor(
     private readonly userTakeBookService: UserTakeBookService,
@@ -26,23 +17,6 @@ export class UserTakeBookController {
     const userId = await this.userToken.verifyUser(headers);
     if (userId) {
       return await this.userTakeBookService.findOne(userId, param);
-  @Get('/:id')
-  @ApiBadRequestResponse()
-  @ApiNotFoundResponse()
-  @ApiHeader({
-    name: 'user_token',
-    description: 'User token',
-    required: true,
-  })
-  @HttpCode(HttpStatus.OK)
-  async findOne(
-    @Param('id') id: string,
-    @Headers() headers: any,
-    @Res() res: Response,
-  ) {
-    const userId = await this.userToken.verifyUser(headers);
-    if (userId) {
-      return await this.userTakeBookService.findOne(id, userId, res);
     }
   }
 }
